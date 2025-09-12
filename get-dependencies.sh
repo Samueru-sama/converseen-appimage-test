4b062c4997e251d0cbd8ef42e6f90ae771180697
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-12 04:12:58 -0400

    needed for custom theming to work

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -30,6 +30,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl --prefer-nano qt6-base-mini libxml2-mini opus-mini
+./get-debloated-pkgs.sh --add-opengl --prefer-nano qt6-base-mini gtk3-mini libxml2-mini opus-mini
 
 pacman -Q converseen | awk '{print $2; exit}' > ~/version
