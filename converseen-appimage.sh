f5550a413172828796af5ce65d544704fed8a25a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-12 08:01:48 +0000

    Add files via upload

---
+++ converseen-appimage.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+VERSION="$(cat ~/version)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+export ADD_HOOKS="self-updater.bg.hook"
+# The arch package does not have icon lol
+export ICON=/usr/share/icons/hicolor/256x256/apps/converseen.png
+export DESKTOP=/usr/share/applications/net.fasterland.converseen.desktop
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=Converseen-"$VERSION"-anylinux-"$ARCH".AppImage
+
+# Deploy dependencies
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/converseen
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
+
